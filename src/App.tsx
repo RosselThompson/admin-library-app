f9af4a38ed06d168e601589f7012f5a7a900149a
Rossel Thompson <rosselthompson@Rossels-MacBook-Pro.local>
2023-06-17 00:07:21 -0600

    feat: add home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Admin, Resource } from 'react-admin';
 import { dataProvider } from './dataProvider';
+import Home  from '@/components/home/home';
 import authors from '@/components/authors/authors';
 import books from '@/components/books/books';
 
 export const App = () => 
-<Admin dataProvider={dataProvider}>
+<Admin dataProvider={dataProvider} dashboard={Home}>
     <Resource {...authors} />
     <Resource {...books} />
 </Admin>;
---
+++ src/components/home/home.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from '@mui/material';
+
+const Home = () => (
+  <Card>
+    <CardHeader title="Welcome to the administration" />
+    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+  </Card>
+);
+
+export default Home;

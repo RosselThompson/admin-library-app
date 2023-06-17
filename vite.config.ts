6448feee8784363f2cc31122e49b40e309bded28
Rossel Thompson <rosselthompson@Rossels-MacBook-Pro.local>
2023-06-16 22:45:32 -0600

    feat: config eslint and prettier formater

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint()],
     define: {
         'process.env': process.env,
     },
@@ -11,4 +13,9 @@ export default defineConfig({
         host: true,
     },
     base: './',
+    resolve: {
+        alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'src') },
+        ],
+      },
 });
